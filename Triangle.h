aeef226c21a3498016167b14f2111f16fd8efb6b
Kolbaskoff <grepd.initd@aol.com>
2022-06-09 21:38:10 +0000

    Code added

--- Triangle.h
+++ Triangle.h
@@ -5,14 +5,26 @@
 #ifndef CLASSES_TRIANGLE_H
 #define CLASSES_TRIANGLE_H
 
+#include "Figure.h"
+
 /**
  * TODO extend this Class from Figure class and Implement constructor, destructor,  perimeter and area methods in Triangle.cpp file
  */
-class Triangle {
+class Triangle : public Figure {
 //TODO put your code here
 //you should define methods and constructor/destructor prototypes here and implement them in Triangle.cpp file
 //constructor must use the pointers to 3 apexes of the triangle.
+private:
+	Point** po;
 
+public:
+
+	Triangle(Point *a, Point *b, Point *c);
+	~Triangle();
+
+
+	int perimeter() override;
+	int area() override;
 };
 
 
